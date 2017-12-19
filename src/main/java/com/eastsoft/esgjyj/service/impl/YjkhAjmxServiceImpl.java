290b5a6055b7105cb2759185e79123a7ffcb2c60
zhzhx <zhangzhenx@eastsoft.com.cn>
2017-12-19 20:51:06 +0800

    更新

--- src/main/java/com/eastsoft/esgjyj/service/impl/YjkhAjmxServiceImpl.java
+++ src/main/java/com/eastsoft/esgjyj/service/impl/YjkhAjmxServiceImpl.java
@@ -1,7 +1,9 @@
 package com.eastsoft.esgjyj.service.impl;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -21,6 +23,8 @@ public class YjkhAjmxServiceImpl implements YjkhAjmxService {
     private YjkhAjmxDao yjkhAjmxDao;
     @Autowired
     private BaseDao baseDao;
+    @Autowired
+    private GySpyjkhServiceImpl gySpyjkhService;
     
     @Override
     public Ajmx get(String id) {
@@ -45,7 +49,19 @@ public class YjkhAjmxServiceImpl implements YjkhAjmxService {
     		lb = (String)item.get("LB");
     		colIndex = (String)item.get("COL_INDEX");
     	}
-    	if(list.size() == 0) return list;
+    	if(list.size() == 0 && "a3".equals(map.get("colIndex"))) {
+    		sql = "select KHID, OFFICEID from YJKH_KHDX where ID = '" + map.get("khdxid") + "' and DXTYPE = '2'";
+    		List<Map<String, Object>> tzList = baseDao.queryForList(sql);
+    		String khid = "", officeid = "";
+    		if(tzList.size() > 0) {
+    			khid = (String)tzList.get(0).get("KHID");
+    			officeid = (String)tzList.get(0).get("OFFICEID");
+    			list =  getBmfgyj(khid, officeid);
+    			return list;
+    		} else {
+    			return list;
+    		}
+    	}
     	Map<String, Object> item = new HashMap<>();
     	item.put("AH", "合计");
     	if("4,6".contains(lb)) {
@@ -81,7 +97,55 @@ public class YjkhAjmxServiceImpl implements YjkhAjmxService {
     	}
     	return list;
     }
-
+    /**
+	 * 部门法官业绩
+	 * @param khid 考核主键
+	 * @return
+	 */
+	public List<Map<String, Object>> getBmfgyj(String khid, String khdxbm) {
+		String sql = "select SCORE, (select USERNAME from S_USER "
+				+ " where USERID = YJKH_KHDX.USERID) as USERNAME "
+				+ " from YJKH_KHJG, YJKH_KHDX where YJKH_KHJG.DXID = YJKH_KHDX.ID "
+				+ " and YJKH_KHDX.DXTYPE = '1' and YJKH_KHDX.OFFICEID = '" 
+				+ khdxbm + "' and YJKH_KHDX.KHID = '" + khid + "'";
+		List<Map<String, Object>> list = baseDao.queryForList(sql);
+		sql = "select COUNT(*) from YJKH_KHDX where KHID = '" + khid + "' and DXTYPE = '1'"
+				+ " and YJKH_KHDX.OFFICEID = '" + khdxbm + "'";
+		int userCnt = baseDao.queryForInt(sql);
+		double cnt = 0.0, score = 0.0;
+		String username = "";
+		Map<String, Map<String, Object>> map = new LinkedHashMap<>();
+		Map<String, Object> fgMap = null;
+		for(Map<String, Object> item : list) {
+			username = (String)item.get("USERNAME");
+			score = ((BigDecimal)item.get("SCORE")).doubleValue();
+			cnt += score;
+			if(map.get(username) == null) {
+				fgMap = new HashMap<>();
+				fgMap.put("AH", username);
+				fgMap.put("DFSM", "法官审判业绩考评得分");
+				fgMap.put("SCORE", score);
+				map.put(username, fgMap);
+			} else {
+				fgMap = map.get(username);
+				fgMap.put("SCORE", fgMap.get("SCORE") == null ? 0.0 : (Double)fgMap.get("SCORE") + score);
+			}
+		}
+		if(list.size() > 0) {
+			double count = cnt / userCnt * 0.5;
+			count = gySpyjkhService.decimal(count);
+			fgMap = new HashMap<>();
+			fgMap.put("AH", "折算后得分");
+			fgMap.put("DFSM", "法官审判业绩考评人均分:" + count * 2 + ";法官审判业绩考评折算后（按50%折算）人均分:" + count);
+			fgMap.put("SCORE", count);
+			map.put("人均分", fgMap);
+		}
+		List<Map<String, Object>> newList = new ArrayList<>();
+		for(Map.Entry<String, Map<String, Object>> entry : map.entrySet()) {
+			newList.add(entry.getValue());
+		}
+		return newList;
+	}
     @Override
     public int count(Map<String, Object> map) {
         return yjkhAjmxDao.count(map);
